ce911fc9160cf7d469674b9192169c5edec23619
LiangCY <liangcynju@qq.com>
2016-04-02 15:19:35 +0800

    调整分页功能

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,20 @@
 var $ = require('jquery');
 
-
 $(function () {
+  var $body = $('body');
 
+  /**
+   * 阻止页面滚动
+   */
   document.documentElement.style.overflow = 'hidden';
   document.body.style.overflow = 'hidden';
+  $body.bind('touchmove', function (e) {
+    e.preventDefault()
+  });
 
-  var $body = $('body');
+  /**
+   * 去除无用信息
+   */
   $('.top-banner-ad-container').remove();
   $('iframe').remove();
   $('header#header').remove();
@@ -20,7 +28,9 @@ $(function () {
   $('.submeta').remove();
   $('.content-footer').remove();
   $('footer.l-footer').remove();
-
+  /**
+   * 删除之后加载的无用信息
+   */
   $body.on('DOMNodeInserted', function (e) {
     if (e.target.tagName.toUpperCase() == 'ASIDE' ||
       e.target.className.indexOf('ad-slot') >= 0) {
@@ -28,10 +38,9 @@ $(function () {
     }
   });
 
-  $body.bind('touchmove', function (e) {
-    e.preventDefault()
-  })
-
+  /**
+   * 分词
+   */
   var $headline = $('h1.content__headline');
   $headline.html($headline.text().replace(/([a-z]+)/ig, "<span>$1</span>"));
 
@@ -50,60 +59,85 @@ $(function () {
     $content.html($content.text().replace(/([a-z]+)/ig, "<span>$1</span>"));
   });
 
+  /**
+   * 添加页码区
+   */
+  var $pagination = $("<div class='my-pagination'></div>");
+  $body.append($pagination);
 
-  $body.css('overflow', 'hidden');
+  /**
+   * 获取页码并添加
+   */
+  var anchors = genPagination();
+  for (var i = 1; i <= anchors.length; i++) {
+    $pagination.append("<span class='my-pagination-item'>" + i + "</span>");
+  }
+  $pagination.on('click', 'span', function (e) {
+    jumpToPage(e.target, anchors);
+  });
 
+});
+
+$(window).on('load', function () {
+  setTimeout(function () {
+    scrollTo(0, 0);
+    $('.my-pagination .my-pagination-item:first-child').addClass('active');
+  }, 0);
+});
+
+function genPagination() {
   var $header = $('header.content__head');
-
-  var top = 0;
-  top = $header.offset().top;
+  var top = $header.offset().top;
 
   var anchors = [];
-
   anchors.push($header);
 
   var pageHeight = document.documentElement.clientHeight;
+  var pageWidth = document.documentElement.clientWidth;
 
   $('.content__article-body').css('padding-bottom', pageHeight + 'px');
-
-  var $pagination = $("<div class='my-pagination'></div>");
-
-  $body.append($pagination);
+  $('.content__article-body span').css('display', 'inline-block');
 
   var $elements = $('#article').find('figure,.content__article-body span');
-
-  $('.content__article-body span').css('display', 'inline-block');
 
   $elements.each(function () {
     if (this.tagName.toUpperCase() == 'FIGURE') {
       var $figure = $(this);
-      if ($figure.offset().top + $figure.height() > anchors[anchors.length - 1].offset().top + pageHeight - 54) {
-        $figure.css('margin-top', $figure.height());
+      if ($figure.offset().top + $figure.outerHeight() > anchors[anchors.length - 1].offset().top + pageHeight - 54) {
+        $figure.css('margin-top', $figure.outerHeight());
+        if ($figure.outerHeight() > pageHeight - 54) {
+          $figure.height(pageHeight - 54);
+          var $image = $figure.find('img');
+          var imgHeight = $image.height();
+          var $figcaption = $figure.find('figcaption.caption');
+          var newImgHeight = pageHeight - 54 - $figcaption.outerHeight();
+          $image.height(newImgHeight).css('width', 'auto');
+          var imgWidth = $image.width();
+          $image.css('margin-left', (pageWidth - imgWidth) / 2 + 'px');
+          $figcaption.css('margin-top', (newImgHeight - imgHeight) + 'px');
+        }
         anchors.push($figure);
       }
     } else if (this.tagName.toUpperCase() == 'SPAN') {
       var $span = $(this);
       if ($span.offset().top != top) {
         top = $span.offset().top;
-        if (top + $span.height() > anchors[anchors.length - 1].offset().top + pageHeight - 54) {
-          $span.css('margin-top', $span.height());
+        if (top + $span.outerHeight() > anchors[anchors.length - 1].offset().top + pageHeight - 54) {
+          $span.css('margin-top', pageHeight + 'px');
           anchors.push($span);
         }
       }
     }
   });
 
-  for (var i = 1; i <= anchors.length; i++) {
-    $pagination.append("<span class='my-pagination-item'>" + i + "</span>");
+  return anchors;
+}
+
+function jumpToPage(item, anchors) {
+  if ($(item).hasClass('active')) {
+    return;
   }
-
-  $pagination.on('click', 'span', function (e) {
-    var num = parseInt($(e.target).text());
-    jump(num);
-  });
-
-  function jump(page) {
-    document.body.scrollTop = anchors[page - 1].offset().top - 2;
-  }
-
-});
+  $(item).addClass('active').siblings().removeClass('active');
+  var num = parseInt($(item).text());
+  document.body.scrollTop = anchors[num - 1].offset().top - 2;
+}
